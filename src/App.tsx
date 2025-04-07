1718d4f7d171f08af7918eec506806ad3018c092
AndrewK0val <andrewkoval2003@gmail.com>
2025-04-07 13:19:21 +0100

    started backend, updated website

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@
 import './App.css'
 import { Separator } from "@/components/ui/separator"
 import { Card, CardContent } from "@/components/ui/card"
-import { Skeleton } from "@/components/ui/skeleton"
-import MqttDisplay from "@/components/MQTTDemo"
 
 // import Gauge from "@/components/gauge"
-
 
 import { ThemeProvider } from "@/components/theme-provider";
 import gcsImage from '@/assets/GCS.png'
@@ -77,21 +74,12 @@ function App() {
           </Card>
           
 
-      <MqttDisplay />
-      
 
       </div>
 
 
     </ThemeProvider>
 
-      <div className="flex flex-col space-y-3">
-        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
-        </div>
-      </div>
 
     </>
   )
